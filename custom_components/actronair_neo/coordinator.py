e7d96b61f5f607f141010932afa237144ea0862c
Chris Bradley <chris.bradley@cy.id.au>
2025-05-11 20:42:36 +1000

    More delay

--- custom_components/actronair_neo/coordinator.py
+++ custom_components/actronair_neo/coordinator.py
@@ -17,7 +17,7 @@ type ActronConfigEntry = ConfigEntry[ActronNeoDataUpdateCoordinator]
 _LOGGER = logging.getLogger(__name__)
 
 SCAN_INTERVAL = timedelta(seconds=30)
-REQUEST_REFRESH_DELAY = 2.0
+REQUEST_REFRESH_DELAY = 3.0
 
 
 class ActronNeoDataUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
